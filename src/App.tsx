bf6ae845ca3f67d7931ca5ee449d9356587d66ed
Akshit1311 <a4748g@gmail.com>
2022-09-06 00:41:15 +0530

    add: styling and instructions to example app

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
   const peers = useRootStore((state) => state.peers);
   const peerId = useRootStore((state) => state.peerId);
   const lobbyPeers = useRootStore((state) => state.lobbyPeers);
+  const roomState = useRootStore((state) => state.roomState);
 
   const handleJoin = async () => {
     try {
@@ -47,55 +48,80 @@ function App() {
 
   return (
     <HuddleClientProvider value={huddleClient}>
-      <div className="App">
+      <div className="App grid grid-cols-2">
         <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={handleJoin}>Join Room</button>
-          <button onClick={() => enableStream()}>Enable Stream</button>
-          <button onClick={() => pauseTracks()}>Disable Stream</button>
-          <button onClick={() => huddleClient.enableWebcam()}>
-            Enable Webcam
-          </button>
-          <button onClick={() => huddleClient.disableWebcam()}>
-            Disable Webcam
-          </button>
-          <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
-            allowAllLobbyPeersToJoinRoom()
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        {!isCamPaused && (
-          <video style={{ width: "50%" }} ref={videoRef} autoPlay muted></video>
-        )}
+          <div>
+            <a href="https://vitejs.dev" target="_blank">
+              <img src="/vite.svg" className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://reactjs.org" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
 
-        <h1>Lobby Peers</h1>
+          <h2 className={`text-${!roomState.joined ? "red" : "green"}`}>
+            Room Joined:&nbsp;{roomState.joined.toString()}
+          </h2>
+          <h2>Instructions</h2>
+          <ol className="w-fit mx-auto text-left">
+            <li>
+              Click on <b>Enable Stream</b>
+            </li>
+            <li>
+              Then Click on <b>Join room</b>, <i>"Room Joined"</i> should be
+              changed to true
+            </li>
+            <li>
+              Open the app in a <b>new tab</b> and repeat <b>steps 1 & 2</b>
+            </li>
+            <li>Return to 1st tab, now you'll see peers in the peer list,</li>
+            <li>
+              Click on <b>allowAllLobbyPeersToJoinRoom</b> to accept peers into
+              the room.
+            </li>
+          </ol>
+        </div>
+
         <div>
-          {lobbyPeers.map((peer) => (
-            <div>{peer.peerId}</div>
-          ))}
+          <div className="card">
+            <button onClick={handleJoin}>Join Room</button>
+            <button onClick={() => enableStream()}>Enable Stream</button>
+            <button onClick={() => pauseTracks()}>Disable Stream</button>
+            <button onClick={() => huddleClient.enableWebcam()}>
+              Enable Webcam
+            </button>
+            <button onClick={() => huddleClient.disableWebcam()}>
+              Disable Webcam
+            </button>
+            <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
+              allowAllLobbyPeersToJoinRoom()
+            </button>
+          </div>
+          {!isCamPaused && (
+            <video
+              style={{ width: "50%" }}
+              ref={videoRef}
+              autoPlay
+              muted
+            ></video>
+          )}
+
+          {lobbyPeers[0] && <h2>Lobby Peers</h2>}
+          <div>
+            {lobbyPeers.map((peer) => (
+              <div>{peer.peerId}</div>
+            ))}
+          </div>
+
+          {Object.values(peers)[0] && <h2>Peers</h2>}
+
+          <div className="peers-grid">
+            {Object.values(peers).map((peer) => (
+              <PeerVideoAudioElem peerIdAtIndex={peer.peerId} />
+            ))}
+          </div>
         </div>
-
-        <h1>Peers</h1>
-
-        <div className="peers-grid">
-          {Object.values(peers).map((peer) => (
-            <PeerVideoAudioElem peerIdAtIndex={peer.peerId} />
-          ))}
-        </div>
-
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </div>
     </HuddleClientProvider>
   );
